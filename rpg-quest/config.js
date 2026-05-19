067aa67530c83921b5ca3f931390fc1b821d2779
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-19 21:13:23 +0300

    add_rpg_game
    start version / image loader

---
+++ rpg-quest/config.js
@@ -0,0 +1,20 @@
+const GALLERY_CONFIG = {
+    // Портреты (для галереи)
+    portraits: [
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/avatars/archer.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/avatars/barbarian.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/avatars/fire_mage.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/avatars/knight.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/avatars/warrior.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/avatars/water_mage.png'
+    ],
+    
+    // Полноростовые спрайты (для Canvas) — индексы совпадают с портретами
+    fullbody: [
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/archer.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/barbarian_m.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/witches.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/warrior.png',
+        'https://github.com/Gabryelf/Atlas-Assets/raw/main/docs/images/fantasy/elementalist.png'
+    ]
+};
